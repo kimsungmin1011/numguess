bfa47670eeb223658e2f45ad49602af49d0d670b
kimsungmin1011 <tata153@naver.com>
2023-01-10 16:30:32 +0900

    Feat: Update game

--- hello.py
+++ hello.py
@@ -7,20 +7,23 @@ answer=randint(0,100)
 username=input("Hi there, What is your name?")
 print(f"Hi, {username}! Please be my guest!")
 
-#Get and print User's guess
-guess=int(input(f"So {username}, Guess the number(1~100): "))
-print(f"Well choice {username}~ You picked {guess}!!")
+while 1:
+    guess=int(input(f"So {username}, Guess the number(1~100): "))
+    print(f"Well choice {username}~ You picked {guess}!!")
 
 #compare answer with user's guess   
-if guess==answer:
-    print('********************')
-    sleep(1)
-    print('********************')
-    sleep(1)
-    print('********************')
-    sleep(1)
-    print(f'You got it right!! The answer is {answer}!!')
-elif guess>answer:
-    print(f'Keep going man~! That was too high {username}')
-elif answer>guess:
-    print(f'Kepp going man~! That was too low {username}')
+    if guess==answer:        
+        print('********************')
+        sleep(1)
+        print('********************')
+        sleep(1)
+        print('********************')
+        sleep(1)
+        print(f'You got it right!! The answer is {answer}!!')
+        break
+    elif guess>answer:
+        print(f'Keep going man~! That was too high {username}')
+        continue
+    elif answer>guess:
+        print(f'Kepp going man~! That was too low {username}')
+        continue
