e7965ce8cad3d6df0e80fd81e62f412e41dbfc24
kimsungmin1011 <tata153@naver.com>
2023-01-10 15:09:41 +0900

    Feat: Add comparing user's guess with answer
    만약, 유저의 입력값이 정답과 같다면축하메시지를 출력한다.
    그렇지 않은 경우 중,
    유저의 값이 정답보다 크다면, 크다는 메시지를
    유저의 값이 정답보다 작다면, 작다는 메시지를 출력한다.

--- hello.py
+++ hello.py
@@ -1,14 +1,26 @@
-import random
-answer=random.randit(1,100)
-print(answer)
+from random import randint
+from time import sleep
+# Print answer for debugging
+answer=randint(0,100)
 
-#Don't do
-animals=['cat','dog','dragon']
+#User interaction
+username=input("Hi there, What is your name?")
+print(f"Hi, {username}! Please be my guest!")
 
-#Do
-menus=[
-    'Cola',
-    'Cutlet',
-    'Ramen',
-    'Salad',
-]
+#Get and print User's guess
+guess=int(input(f"So {username}, Guess the number(1~100): "))
+print(f"Well choice {username}~ You picked {guess}!!")
+
+#compare answer with user's guess   
+if guess==answer:
+    print('********************')
+    sleep(1)
+    print('********************')
+    sleep(1)
+    print('********************')
+    sleep(1)
+    print(f'You got it right!! The answer is {answer}!!')
+elif guess>answer:
+    print(f'Keep going man~! That was too high {username}')
+elif answer>guess:
+    print(f'Kepp going man~! That was too low {username}')
